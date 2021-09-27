af015e1a2fbf2c2a38bc343643847a634ea0ce4c
sam.rose@ttigroupna.com <sam.rose@Sam-Rose-MBP.wowway.com>
2021-09-26 21:34:36 -0400

    update to latest 3.x series release of jena

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
                  "jenarepo" {:url "https://repository.apache.org/content/repositories/releases/"}} 
   :license {:name "GNU Lesser General Public License v3.0"
             :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.apache.jena/jena-core "3.15.0"]
-                 [org.apache.jena/jena-arq "3.15.0"]
-                 [org.apache.jena/jena-iri "3.15.0"]
-                 [org.apache.jena/jena-tdb "3.15.0"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.apache.jena/jena-core "3.17.0"]
+                 [org.apache.jena/jena-arq "3.17.0"]
+                 [org.apache.jena/jena-iri "3.17.0"]
+                 [org.apache.jena/jena-tdb "3.17.0"]]
   :main ^:skip-aot sparql.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
